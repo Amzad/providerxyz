97d6f4cd2d6fefcd6b272faf76997e8dc6830dee
fred belotte <fredbelotte@outlook.com>
2019-08-18 11:28:36 -0500

    add new aspnet solution, projects

--- aspnet/Xyz.Provider.Api/Program.cs
+++ aspnet/Xyz.Provider.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace HousingXyz.Provider.Api
+namespace Xyz.Provider.Api
 {
     public class Program
     {
--- aspnet/Xyz.Provider.Api/Startup.cs
+++ aspnet/Xyz.Provider.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace HousingXyz.Provider.Api
+namespace Xyz.Provider.Api
 {
     public class Startup
     {
